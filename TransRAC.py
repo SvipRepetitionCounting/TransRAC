5f96b91ba3e2011e5404bf0581d8e1c58f7414aa
957001934 <957001934@qq.com>
2022-02-11 14:57:18 +0800

    change TransRAC model

--- TransRAC.py
+++ TransRAC.py
@@ -1,4 +1,4 @@
-"""TransRAC model """
+"""TransRAC network"""
 from mmcv import Config
 from mmaction.models import build_model
 from mmcv.runner import load_checkpoint
@@ -24,7 +24,6 @@ class attention(nn.Module):
         # q: [B, head, F, model_dim]
         scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.scale)  # [B,Head, F, F]
         if attn_mask:
-            # 给需要mask的地方设置一个负无穷
             scores = scores.masked_fill_(attn_mask, -np.inf)
         scores = self.softmax(scores)
         scores = self.dropout(scores)  # [B,head, F, F]
@@ -33,6 +32,7 @@ class attention(nn.Module):
 
 
 class Similarity_matrix(nn.Module):
+    '''buliding similarity matrix by self-attention mechanism '''
 
     def __init__(self, num_heads=4, model_dim=512):
         super().__init__()
@@ -50,7 +50,6 @@ class Similarity_matrix(nn.Module):
         # self.layer_norm = nn.LayerNorm(model_dim)
 
     def forward(self, query, key, value, attn_mask=None):
-        # 残差连接
         batch_size = query.size(0)
         # dim_per_head = self.dim_per_head
         num_heads = self.num_heads
@@ -60,9 +59,9 @@ class Similarity_matrix(nn.Module):
         value = self.linear_v(value)
         # split by heads
         # [B,F,model_dim] ->  [B,F,num_heads,per_head]->[B,num_heads,F,per_head]
-        query = query.view(batch_size, -1, num_heads, self.model_dim // self.num_heads).transpose(1, 2)
-        key = key.view(batch_size, -1, num_heads, self.model_dim // self.num_heads).transpose(1, 2)
-        value = value.view(batch_size, -1, num_heads, self.model_dim // self.num_heads).transpose(1, 2)
+        query = query.reshape(batch_size, -1, num_heads, self.model_dim // self.num_heads).transpose(1, 2)
+        key = key.reshape(batch_size, -1, num_heads, self.model_dim // self.num_heads).transpose(1, 2)
+        value = value.reshape(batch_size, -1, num_heads, self.model_dim // self.num_heads).transpose(1, 2)
         # similar_matrix :[B,H,F,F ]
         matrix = self.attention(query, key, value, attn_mask)
 
@@ -89,7 +88,9 @@ class PositionalEncoding(nn.Module):
 
 
 class TransEncoder(nn.Module):
-    def __init__(self, d_model, n_head, dim_ff, dropout=0.0, num_layers=1,num_frames=64):
+    '''standard transformer encoder'''
+
+    def __init__(self, d_model, n_head, dim_ff, dropout=0.0, num_layers=1, num_frames=64):
         super(TransEncoder, self).__init__()
         self.pos_encoder = PositionalEncoding(d_model, 0.1, num_frames)
 
@@ -108,7 +109,7 @@ class TransEncoder(nn.Module):
 
 
 class Prediction(nn.Module):
-    ''' 全连接预测网络 '''
+    ''' predict the density map with densenet '''
 
     def __init__(self, input_dim, n_hidden_1, n_hidden_2, out_dim):
         super(Prediction, self).__init__()
@@ -124,32 +125,36 @@ class Prediction(nn.Module):
         )
 
     def forward(self, x):
-        # x = x.flatten(start_dim=1)
         x = self.layers(x)
         return x
 
 
 class TransferModel(nn.Module):
-    def __init__(self, config, checkpoint, num_frames,scales):
+    def __init__(self, config, checkpoint, num_frames, scales, OPEN=False):
         super(TransferModel, self).__init__()
         self.num_frames = num_frames
         self.config = config
         self.checkpoint = checkpoint
-        self.scales = scales  # 目前的尺度为1
-        self.backbone = self.load_model()
-        self.Replication_padding1 = nn.ConstantPad3d((0, 0, 0, 0, 1, 1),0)
-        self.Replication_padding2 = nn.ConstantPad3d((0, 0, 0, 0, 2, 2),0)
-        self.Replication_padding4 = nn.ConstantPad3d((0, 0, 0, 0, 4, 4),0)
+        self.scales = scales
+        self.OPEN = OPEN
+
+        self.backbone = self.load_model()  # load pretrain model
+
+        self.Replication_padding1 = nn.ConstantPad3d((0, 0, 0, 0, 1, 1), 0)
+        self.Replication_padding2 = nn.ConstantPad3d((0, 0, 0, 0, 2, 2), 0)
+        self.Replication_padding4 = nn.ConstantPad3d((0, 0, 0, 0, 4, 4), 0)
+
         self.conv3D = nn.Conv3d(in_channels=768,
                                 out_channels=512,
                                 kernel_size=3,
                                 padding=(3, 1, 1),
                                 dilation=(3, 1, 1))
+
         self.bn1 = nn.BatchNorm3d(512)
         self.SpatialPooling = nn.MaxPool3d(kernel_size=(1, 7, 7))
 
         self.sims = Similarity_matrix()
-        self.conv3x3 = nn.Conv2d(in_channels=4 * len(self.scales),  # num_head*scale_num 
+        self.conv3x3 = nn.Conv2d(in_channels=4 * len(self.scales),  # num_head*scale_num
                                  out_channels=32,
                                  kernel_size=3,
                                  padding=1)
@@ -157,61 +162,78 @@ class TransferModel(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
 
         self.dropout1 = nn.Dropout(0.25)
-        self.input_projection = nn.Linear(self.num_frames * 32, 512)  #
+        self.input_projection = nn.Linear(self.num_frames * 32, 512)  # 线性投射层
         self.ln1 = nn.LayerNorm(512)
 
-        self.transEncoder = TransEncoder(d_model=512, n_head=4, dropout=0.2, dim_ff=512, num_layers=1,num_frames=self.num_frames)
-        self.FC = Prediction(512, 512, 256, 1)  # 1104 输出为1
+        self.transEncoder = TransEncoder(d_model=512, n_head=4, dropout=0.2, dim_ff=512, num_layers=1,
+                                         num_frames=self.num_frames)
+        self.FC = Prediction(512, 512, 256, 1)  #
 
     def load_model(self):
+        ### load  pretrained model of video swin transformer using mmaction and mmcv API
         cfg = Config.fromfile(self.config)
         model = build_model(cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
-        load_checkpoint(model, self.checkpoint, map_location='cpu')
+
+        ### load hyperparameters by mmcv api
+        # load_checkpoint(model, self.checkpoint, map_location='cpu')
+        # backbone = model.backbone
+
+        ### load hyperparameters by pytorch
+        loaded_ckpt = torch.load(self.checkpoint)
         backbone = model.backbone
+        net_dict = backbone.state_dict()
+        state_dict = {k: v for k, v in loaded_ckpt.items() if k in net_dict.keys()}
+        net_dict.update(state_dict)
+        backbone.load_state_dict(net_dict, strict=False)
+
         print('--------- backbone loaded ------------')
+
         return backbone
 
-    def forward(self, x, epoch):
+    def forward(self, x):
+        # x: tensor([batch_size, channel, temporal_dim, height, width])
         with autocast():
             batch_size, c, num_frames, h, w = x.shape
-            # scales = [1,4,8]  # 目前的尺度为1
+            # scales = [1,4,8]
+            ### We currently only support 1, 4, 8 flames. If you want to add more scale, you can change the part and don't forget padding.
             multi_scales = []
             for scale in self.scales:
                 if scale == 4:
                     x = self.Replication_padding2(x)
                     crops = [x[:, :, i:i + scale, :, :] for i in
-                             range(0, self.num_frames - scale + scale//2 * 2, max(scale // 2, 1))]
+                             range(0, self.num_frames - scale + scale // 2 * 2, max(scale // 2, 1))]
                 elif scale == 8:
                     x = self.Replication_padding4(x)
                     crops = [x[:, :, i:i + scale, :, :] for i in
-                             range(0, self.num_frames - scale + scale//2 * 2, max(scale // 2, 1))]
+                             range(0, self.num_frames - scale + scale // 2 * 2, max(scale // 2, 1))]
                 else:
                     crops = [x[:, :, i:i + 1, :, :] for i in range(0, self.num_frames)]
 
                 slice = []
-                if epoch <200:
+                ## feature extract with video SwinTransformer
+                if not self.OPEN:
                     with torch.no_grad():
                         for crop in crops:
-                            crop = self.backbone(crop)  # ->[batch_size, 768, scale/2(up), 7, 7]  帧过vst
+                            crop = self.backbone(crop)  # ->[batch_size, 768, scale/2(up), 7, 7]
                             slice.append(crop)
-                else:
+                else:  # train  the feature extractor (video SwinTransformer backbone)
                     for crop in crops:
-                        crop = self.backbone(crop)  # ->[batch_size, 768, scale/2(up), 7, 7]  帧过vst
+                        crop = self.backbone(crop)  # ->[batch_size, 768, scale/2(up), 7, 7]
                         slice.append(crop)
 
-                x_scale = torch.cat(slice, dim=2)  # ->[b,768,f,size,size]
+                x_scale = torch.cat(slice, dim=2)  # -> [b,768,f,size,size]
                 x_scale = F.relu(self.bn1(self.conv3D(x_scale)))  # ->[b,512,f,7,7]
                 # print(x_scale.shape)
                 x_scale = self.SpatialPooling(x_scale)  # ->[b,512,f,1,1]
                 x_scale = x_scale.squeeze(3).squeeze(3)  # -> [b,512,f]
                 x_scale = x_scale.transpose(1, 2)  # -> [b,f,512]
-                x_scale = x_scale.reshape(batch_size, self.num_frames, -1)  # -> [b,f,512]
 
                 # -------- similarity matrix ---------
                 x_sims = F.relu(self.sims(x_scale, x_scale, x_scale))  # -> [b,4,f,f]
                 multi_scales.append(x_sims)
 
             x = torch.cat(multi_scales, dim=1)  # [B,4*scale_num,f,f]
+            ## x are the similarity matrixs
             x_matrix = x
             x = F.relu(self.bn2(self.conv3x3(x)))  # [b,32,f,f]
             x = self.dropout1(x)
@@ -227,30 +249,6 @@ class TransferModel(nn.Module):
             x = x.transpose(0, 1)  # ->[b,f, 512]
 
             x = self.FC(x)  # ->[b,f,1]
-
-            x = x.view(batch_size, self.num_frames)
+            x = x.squeeze(2)
 
             return x, x_matrix
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
