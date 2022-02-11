b2f84278dd2cebc8b0fc8444740476c96f166171
957001934 <957001934@qq.com>
2022-02-11 16:05:00 +0800

    update label_norm

--- label_norm.py
+++ label_norm.py
@@ -1,3 +1,4 @@
+""" transform label to groundtruth(density map)"""
 from scipy import integrate
 import math
 import numpy as np
@@ -7,7 +8,6 @@ def PDF(x, u, sig):
     # f(x)
     return np.exp(-(x - u) ** 2 / (2 * sig ** 2)) / (math.sqrt(2 * math.pi) * sig)
 
-
 # integral f(x)
 def get_integrate(x_1, x_2, avg, sig):
     y, err = integrate.quad(PDF, x_1, x_2, args=(avg, sig))
@@ -15,8 +15,8 @@ def get_integrate(x_1, x_2, avg, sig):
 
 
 def normalize_label(y_frame, y_length):
-    # y_length: 帧总数
-    # return: normalize_label  size:narray(y_length,)
+    # y_length: total frames
+    # return: normalize_label  size:nparray(y_length,)
     y_label = [0 for i in range(y_length)]  # 坐标轴长度，即帧数
     for i in range(0, len(y_frame), 2):
         x_a = y_frame[i]
